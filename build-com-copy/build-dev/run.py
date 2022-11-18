886b65ffd1f8af7e0cd96d75379861e2bdc10b97
Igor Rubio <ighor_souza@hotmail.com>
2022-11-18 17:09:15 -0300

    Criacao de um servidor WEB em python

---
+++ build-com-copy/build-dev/run.py
@@ -0,0 +1,27 @@
+#Servidor web em Python
+import logging
+import http.server
+import socketserver
+import  getpass
+
+class MyHTTPHandler(http.server.SimpleHTTPRequestHandler):
+    def log_message(self, format, *args):
+      logging.info("%s - - [%s] %s\n"% (
+        self.client_address[0],
+        self.log_date_time_string(),
+        format%args
+      ))
+
+      loggin.basicConfig(
+        filename='/log/http-server.log',
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+      )
+      loggin.getLogger().addHandler(logging.StreamHandler())
+      loggin.info('Inicializando...')
+      PORT = 8000
+
+      httpd = socketserver.TCPServer(("",PORT), MyHTTPHandler)
+      logging.info('Escutando  porta: %s', PORT)
+      logging.info('Usuario: %s', getpass.getuser())
+      httpd.socketserver_forever()
