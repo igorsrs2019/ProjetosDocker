195c4ea2feb20be303bae9ef3b71ae9bc6425ad8
Igor Rubio <ighor_souza@hotmail.com>
2022-11-21 11:59:37 -0300

    Criacao de um server web em python

--- build-dev/run.py
+++ build-dev/run.py
@@ -1,27 +1,26 @@
-#Servidor web em Python
 import logging
 import http.server
 import socketserver
-import  getpass
+import getpass
 
 class MyHTTPHandler(http.server.SimpleHTTPRequestHandler):
     def log_message(self, format, *args):
-      logging.info("%s - - [%s] %s\n"% (
-        self.client_address[0],
-        self.log_date_time_string(),
-        format%args
-      ))
+        logging.info("%s - - [%s] %s\n"% (
+            self.client_address[0],
+            self.log_date_time_string(),
+            format%args
+        ))
 
-      loggin.basicConfig(
-        filename='/log/http-server.log',
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        level=logging.INFO
-      )
-      loggin.getLogger().addHandler(logging.StreamHandler())
-      loggin.info('Inicializando...')
-      PORT = 8000
+logging.basicConfig(
+    filename='/log/http-server.log',
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logging.getLogger().addHandler(logging.StreamHandler())
+logging.info('inicializando...')
+PORT = 8000
 
-      httpd = socketserver.TCPServer(("",PORT), MyHTTPHandler)
-      logging.info('Escutando  porta: %s', PORT)
-      logging.info('Usuario: %s', getpass.getuser())
-      httpd.socketserver_forever()
+httpd = socketserver.TCPServer(("", PORT), MyHTTPHandler)
+logging.info('escutando a porta: %s', PORT)
+logging.info('usuário: %s', getpass.getuser())
+httpd.serve_forever()
