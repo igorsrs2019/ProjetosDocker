96070842b017b394c004f618672b4165202b8045
Igor Rubio <ighor_souza@hotmail.com>
2022-11-22 17:07:52 -0300

    Criacao de API

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -2,15 +2,30 @@ const express = require('express')
 const restful = require('node-restful')
 const server = express()
 const mongoose = restful.mongoose
-//const bodyParser = require('body-parser')
-//const cors = require('cors')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // Database
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
-//Teste
-server.get('/', (req, res, next) => res.send('Backend'))
+//Middlewares
+server.use(bodyParser.urlencoded({extends:true}))
+server.use(bodyParser.json())
+server.use(cors())
 
 //Start Server
 server.listen(3000)
+
+//ODM
+const Client = restful.model('Client',  {
+    name: { type: String, required: true }
+})
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runvalidators: true})
+
+
+//Routes
+Client.register(server, '/clients')
